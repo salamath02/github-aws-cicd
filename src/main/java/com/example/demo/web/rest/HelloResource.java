ff0c53fbd13d46c3313e6f75e71326a36b95724b
szubairali <szubairali@elm.sa>
2026-02-04 15:08:32 +0300

    new service for greet by id

--- src/main/java/com/example/demo/web/rest/HelloResource.java
+++ src/main/java/com/example/demo/web/rest/HelloResource.java
@@ -2,6 +2,7 @@ package com.example.demo.web.rest;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class HelloResource {
     @GetMapping
     public ResponseEntity<String> greet() {
         return ResponseEntity.ok("Welcome to Spring EC2");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> greetById(@PathVariable("id") String id) {
+        return ResponseEntity.ok("Hello, your ID is: " + id);
     }
 }
